61605181c3d7d9fce95d700802b8e703a2de2e18
Milena <milykar13@icloud.com>
2020-12-10 11:47:29 +0000

    player profile

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const router = require("express").Router();
 const authRoutes = require("./auth");
-const playerRoutes = require("./userPlayer.js");
-const organizerRoutes = require("./userOrganizer.js");
-const eventsRoutes = require("./events");
+const playerRoutes = require("./userPlayer");
+//const organizerRoutes = require("./userOrganizer");
+//const eventsRoutes = require("./events");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+
 router.use("/auth", authRoutes);
 router.use("/player", playerRoutes);
-router.use("/organizer", organizerRoutes);
+//router.use("/organizer", organizerRoutes);
 
 module.exports = router;
